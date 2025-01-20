71d494f2f886363901af8aafa14cf5b8be131d60
doyeon <doyeon@doyeon.local>
2025-01-20 13:42:16 +0900

    fish

--- setup.sh
+++ setup.sh
@@ -9,13 +9,19 @@ softwareupdate --install-rosetta
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # brew packages
-brew install git stow tree starship gh neovim btop bat tmux ripgrep pip3
+brew install git stow tree starship gh neovim btop bat tmux ripgrep lazygit fzf fish eza python3 ollama
+
+# set default shell to fish
+curl -L https://get.oh-my.fish | fish
+sudo bash -c 'echo $(which fish) >> /etc/shells'
+chsh -s $(which fish)
 
 # brew applications
-brew install --cask ghostty wireshark raycast docker nikitabobko/tap/aerospace
+brew install --cask ghostty wireshark raycast docker ollama nikitabobko/tap/aerospace
 
-# bring git dotfiles
+# setup github
 git clone https://github.com/doyeonx/dotfiles.git
 
 # sync dotfiles
+cd ~/dotfiles
 stow .
